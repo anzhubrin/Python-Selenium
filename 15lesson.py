efbf183ca4e115cf7752800c4eef38a80742b2b2
anzhubrin <1111>
2024-05-03 13:53:33 +0100

    done lesson 15

---
+++ 15lesson.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("https://demoqa.com/selectable")
+driver.find_element(By.XPATH, "//a[@id='demo-tab-grid']").click()
+
+driver.find_element(By.XPATH, "//div[@id='row1']/li[1]").click()
+driver.find_element(By.XPATH, "//div[@id='row1']/li[2]").click()
+
+
+driver.find_element(By.XPATH, "//div[@id='row1']/li[1]").get_attribute("active")
+driver.find_element(By.XPATH, "//div[@id='row1']/li[2]").get_attribute("active")
