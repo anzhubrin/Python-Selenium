b6351687428f2bc5abe8b37249d6405b36f6915b
anzhubrin <1111>
2024-05-03 15:38:00 +0100

    check from terminal

--- 15lesson.py
+++ 15lesson.py
@@ -5,10 +5,8 @@ driver = webdriver.Chrome()
 
 driver.get("https://demoqa.com/selectable")
 driver.find_element(By.XPATH, "//a[@id='demo-tab-grid']").click()
-
 driver.find_element(By.XPATH, "//div[@id='row1']/li[1]").click()
 driver.find_element(By.XPATH, "//div[@id='row1']/li[2]").click()
-
 
 driver.find_element(By.XPATH, "//div[@id='row1']/li[1]").get_attribute("active")
 driver.find_element(By.XPATH, "//div[@id='row1']/li[2]").get_attribute("active")
